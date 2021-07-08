274e8b1bf64f94f7bc1b289e12d1bdf0f6c98757
Badoiu Mihail <badoiumihail@gmail.com>
2021-07-08 23:25:01 +0300

    Rearranged the folder structure a bit

--- DataStructures/Trees/BinarySearchTrees/bst.js
+++ DataStructures/Trees/BinarySearchTrees/bst.js
@@ -86,16 +86,3 @@ class BinarySearchTree {
 }
 
 const tree = new BinarySearchTree();
-tree.insert(5);
-tree.insert(2);
-tree.insert(7);
-tree.insert(4);
-tree.insert(1);
-tree.insert(6);
-console.log(tree.find(7));
-console.log(tree.find(2));
-console.log(tree.find(12312));
-console.log(tree.contains(7));
-console.log(tree.contains(2));
-console.log(tree.contains(12312));
-console.log(tree);
