f79002087f4c6190fe9c9c371d368d52ce2be3b3
Badoiu Mihail <badoiumihail@gmail.com>
2021-07-12 22:13:16 +0300

    cleanup dijkstra.js - unimportant

--- Dijkstra/dijkstra.js
+++ Dijkstra/dijkstra.js
@@ -242,25 +242,9 @@ class WeightedGraph {
                 });
             }
         }
+        // add the starting vertex and reverse the order of the path
         return path.concat(smallest).reverse();
     }
 }
 
 const graph = new WeightedGraph();
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('C');
-graph.addVertex('D');
-graph.addVertex('E');
-graph.addVertex('F');
-graph.addEdge('A', 'B', 4);
-graph.addEdge('A', 'C', 2);
-graph.addEdge('B', 'E', 3);
-graph.addEdge('C', 'D', 2);
-graph.addEdge('C', 'F', 4);
-graph.addEdge('D', 'E', 3);
-graph.addEdge('D', 'F', 1);
-graph.addEdge('E', 'F', 1);
-// console.log(graph.adjacencyList);
-console.log(graph.adjacencyList);
-console.log(graph.dijkstra('A','E'));
